e4c2788036b2fb44fc787f46e7a44c82e4b75618
ijnuemik <43838237+ijnuemik@users.noreply.github.com>
2020-03-31 11:40:00 +0900

    2019 카카오 개발자 겨울 인턴쉽

---
+++ 2019Kakao-Winter-Internship-3.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+vector<string> user_id = { "frodo", "fradi", "crodo", "abc123", "frodoc" };
+vector<string> banned_id = { "fr*d*", "*rodo", "******", "******" };
+set<set<string>> s;
+bool check[100];
+
+void go(int index, vector<string> vec, int usrsize, int bansize) {
+
+	if (index == bansize) {
+		set<string> tmp;
+		int problem = 0;
+		for (int i = 0; i < vec.size(); i++) {
+			string banstr = banned_id[i];
+			string usrstr = vec[i];
+			if (banstr.length() == usrstr.length()) {
+				for (int j = 0; j < banstr.length(); j++) {
+					if (banstr[j] != '*') {
+						if (banstr[j] != usrstr[j]) {
+							problem = 1;
+							break;
+						}
+					}
+				}
+			}
+			else {
+				problem = 1;
+				break;
+			}
+			if (problem == 1) break;
+		}
+		if (problem == 0) {
+			for (int i = 0; i < vec.size(); i++) {
+				tmp.insert(vec[i]);
+				cout << vec[i] << " ";
+			}
+			cout << endl;
+			s.insert(tmp);
+		}
+		return;
+	}
+
+	for (int i = 0; i < usrsize; i++) {
+		if (check[i] != true) {
+			check[i] = true;
+			vector<string> ttmp = vec;
+			ttmp.push_back(user_id[i]);
+			go(index + 1, ttmp, usrsize, bansize);
+			check[i] = false;
+		}
+	}
+
+}
+
+int main() {
+
+	int usrsize = user_id.size();
+	int bansize = banned_id.size();
+	vector<string> vec;
+	go(0, vec, usrsize, bansize);
+	cout << s.size() << endl;
+}
